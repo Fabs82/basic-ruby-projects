cd4c5930c16d569218dd4da6f7797d1e0a14e7be
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-06-18 10:39:16 +0200

    Solve substrings using the "reduce" method

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,6 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-# First solution using each
+
+# Solution 1 using EACH
 def substrings(string, dictionary)
     my_hash = Hash.new
     dictionary.each do |word|
@@ -11,4 +12,16 @@ def substrings(string, dictionary)
     my_hash
 end
 
+# Solution 2 using REDUCE
+def substrings_two(string, dictionary)
+    dictionary.reduce(Hash.new(0)) do |accumulator, current_word|
+        count = string.downcase.scan(current_word).count
+        if count > 0
+            accumulator[current_word] = count
+        end
+    accumulator
+    end
+end
+
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings_two("Howdy partner, sit down! How's it going?", dictionary)
